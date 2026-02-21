b093c796fdbefc7435fc80f70334170ca8131281
Tushar Saini <tusharsaini.in@gmail.com>
2026-02-21 20:51:33 +0530

    Add the Project in Java

---
+++ src/Task2/printPyramid.java
@@ -0,0 +1,9 @@
+package Task2;
+
+public class printPyramid {
+    public static void main(String[] args) {
+        System.out.println("*\n**\n***\n****\n*****\n");
+        System.out.println("*****\n****\n***\n**\n*\n");
+        System.out.println("    *\n   **\n  ***\n ****\n*****\n");
+    }
+}
