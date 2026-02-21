e6bde74ff46efb6f52ec0ceab0220555f5b5e372
Tushar Saini <tusharsaini.in@gmail.com>
2026-02-21 20:50:41 +0530

    Add the Project in Java

---
+++ src/Task1/OutputTask.java
@@ -0,0 +1,31 @@
+package Task1;
+
+public class OutputTask {
+    public static void main(String[] arg){
+        //Output
+        System.out.println("Good Morning");
+        System.out.println("Hii");
+
+        //Right Half
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+        System.out.println("*****\n");
+
+        //Reverse Right Half
+        System.out.println("*****");
+        System.out.println("****");
+        System.out.println("***");
+        System.out.println("**");
+        System.out.println("*");
+
+        //Left Half
+        System.out.println("    *");
+        System.out.println("   **");
+        System.out.println("  ***");
+        System.out.println(" ****");
+        System.out.println("*****");
+
+    }
+}
---
+++ src/Test.java
@@ -0,0 +1,7 @@
+public class Test {
+    public static void main(String [] args){
+        float a=87.9F;
+        double b=a;
+        System.out.println(b);
+    }
+}
